d369765e3dfb8b83a9996d233b093139d5eeb394
farrukhbek26 <fsakhatov@gmail.com>
2023-07-15 03:55:37 +0900

    Updateddd

---
+++ src/mock.js
@@ -0,0 +1,10 @@
+export const student = [
+    {id:1, name: 'Farrukh', status: 'IT'},
+    {id:2, name: 'Bobur', status: 'BS'},
+    {id:3, name: 'Nuriddin', status: 'BM'},
+    {id:4, name: 'Bahtiyor', status: 'DS'},
+    {id:5, name: 'Sherzod', status: 'CE'},
+    {id:6, name: 'Azizbek', status: 'CL'},
+    {id:7, name: 'Nodir', status: 'CM'},
+
+]
